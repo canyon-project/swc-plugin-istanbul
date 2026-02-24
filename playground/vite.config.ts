78018d5955295c1b42a28805488f7d34ac6b1c9c
Travis Zhang <wr.zhang25@gmail.com>
2026-02-24 14:31:09 +0800

    chore: daily development

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,15 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react({
-    plugins:[
-        ['ss',{}]
+    plugins: [
+      [path.resolve(__dirname, '..', 'target', 'wasm32-wasip1', 'release', 'swc_plugin_coverage.wasm'), {}]
     ]
   })],
 })
