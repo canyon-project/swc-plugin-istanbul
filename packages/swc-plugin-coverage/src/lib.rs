1f7cdf3cbda686e791f008541e4403569bdd1db2
Travis Zhang <wr.zhang25@gmail.com>
2026-02-24 14:51:28 +0800

    chore: daily development

--- packages/swc-plugin-coverage/src/lib.rs
+++ packages/swc-plugin-coverage/src/lib.rs
@@ -1,6 +1,6 @@
 use swc_core::ecma::ast::Program;
-use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 use swc_core::plugin::metadata::TransformPluginMetadataContextKind;
+use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 use swc_coverage_instrument::create_coverage_instrumentation_visitor;
 
 #[plugin_transform]
